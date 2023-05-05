014689af3eeb5ba4b604a8a03e28614590dacc78
Manuel-Ventura-Work <manuelventurawork@gmail.com>
2023-05-05 11:16:31 -0300

    cambio al main.jsx para utilizar router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+ 
+const Router = createBrowserRouter ([
+  { path: "/",element: <App/>}
+ ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={Router}/>
   </React.StrictMode>,
 )
