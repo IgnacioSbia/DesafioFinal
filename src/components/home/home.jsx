e1939ddbe101d4fc48ceeb205b73162d712bfaa9
Ignacio <ignaciosabiajes@gmail.com>
2023-05-08 09:57:28 -0300

    First Push in Branch

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import refresh from './Images/Refresh.svg'
+import notify from './Images/Notify.svg'
+import './Home.css'
+import musicCupid from './Images/MusicalCupid.svg'
+import conextMusic from './Images/contexMusic.svg'
 
 function Home() {
   return (
-    <div><h1>Música ya</h1></div>
+    <>
+        <header className='homeHeader'>
+            <div className='homeHeaderTitle'>
+             <h1>Música ya</h1>
+            </div>
+
+            <div className='homeHeaderButtons'>
+              <button className='homeButton1'><img src={refresh}/></button>
+              <button className='homeButton2'><img src={notify}/> </button>
+            </div>
+        </header>
+
+        <main className='homeMain'>
+          <div className='homeMusicalCupid'>
+            <div>
+              <img src={musicCupid}/>
+              
+            </div>
+            <div className='homeMusicalCupidText'>
+              <h2 className='homeContentTitle'>Cupido Musical</h2>
+              <p>Tus artistas favoritos nunca van a dejarte con el rozaón roto.</p>
+            </div>
+          </div>
+          <div>
+          <img src={conextMusic}/>
+          </div>
+        </main>
+    </>
   )
 }
 
