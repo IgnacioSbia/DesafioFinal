510b24cb46a5096e23fb14bdd6fd535e93821b4a
Manuel-Ventura-Work <manuelventurawork@gmail.com>
2023-05-08 09:20:28 -0300

    navbar 90%

--- src/components/NavigationBar/NavBar.jsx
+++ src/components/NavigationBar/NavBar.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import './NavBar.css'
+import homeIcon from './Icons-Images/homeinactive.svg'
+import homeIconActive from './Icons-Images/homeActive.svg'
+import friendsIcon from './Icons-Images/friendsInactive.svg'
+import friendsIconActive from './Icons-Images/friendsActive.svg'
+import profileIcon from './Icons-Images/profileInactive.svg'
+import profileIconActive from './Icons-Images/profileActive.svg'
+import searchIcon from './Icons-Images/searchInactive.svg'
+import searchIconActive from './Icons-Images/searchActive.svg'
+import { Link, useLocation } from 'react-router-dom'
+
 
 function NavBar() {
+  const location = useLocation();
+
+  //Cambiar icono home dependiendo de la URL
+  let navIconHome;
+  if (location.pathname === '/navtest1') {
+    navIconHome = homeIconActive;
+  } else navIconHome = homeIcon
+  // Cambiar icono search
+  let navIconSearch;
+  if (location.pathname === '/navtest2') {
+    navIconSearch = searchIconActive;
+  } else navIconSearch = searchIcon
+  //Cambiar icono profile
+  let navIconProfile;
+  if (location.pathname === '/navtest3') {
+    navIconProfile = profileIconActive;
+  } else navIconProfile = profileIcon
+  //Cambiar icono friends
+  let navIconFriends;
+  if (location.pathname === '/navtest4') {
+    navIconFriends = friendsIconActive;
+  } else navIconFriends = friendsIcon
+
+
+  
   return (
-    <div>NavBar</div>
+    <div className='navMainDiv'>
+      <Link to='/navtest1'>
+      <div className={`navHome ${location.pathname !== '/navtest1' ? 'navInactive' : ''}`}><img src={navIconHome}></img>Inicio</div>
+      </Link>
+      <Link to='/navtest2'>
+      <div className={`navSearch ${location.pathname !== '/navtest2' ? 'navInactive' : ''}`}><img  src={navIconSearch} ></img>Buscador</div>
+      </Link>
+      <Link to='/navtest3'>
+      <div className={`navProfile ${location.pathname !== '/navtest3' ? 'navInactive' : ''}`}><img  src={navIconProfile} ></img>Perfil</div>
+      </Link>
+      <Link to='/navtest4'>
+      <div className={`navFriends ${location.pathname !== '/navtest4' ? 'navInactive' : ''}`}><img src={friendsIcon} ></img>Amigos</div>
+      </Link>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+//Eliminar despues, es solo para resting
+import NavBar from './components/NavigationBar/NavBar.jsx'
 
  
 const Router = createBrowserRouter ([
-  { path: "/",element: <App/>}
+  { path: "/",element: <App/>},
+  //eliminar navtest, es solo para testing
+  { path: "/navtest1",element: <NavBar/>},
+  { path: "/navtest2",element: <NavBar/>},
+  { path: "/navtest3",element: <NavBar/>},
+  { path: "/navtest4",element: <NavBar/>}
  ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
