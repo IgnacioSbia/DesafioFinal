82508380f9e718b9e80a60991d29447d1597908b
marianoelcorrea <mcorrealaborde@gmail.com>
2023-05-08 08:11:42 -0300

    Trabajando en signIn

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from './components/SignIn/SignIn.jsx';
+
+function App() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
