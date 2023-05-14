b6d9f5a5dc10b36f36845108c8f9c14aefb97282
Ignacio <ignaciosabiajes@gmail.com>
2023-05-14 01:13:30 -0300

    CarouselCupid y PlaylistByCupid terminados

--- src/components/MusicalCupidCarousel/MusicalCupidCarousel.jsx
+++ src/components/MusicalCupidCarousel/MusicalCupidCarousel.jsx
@@ -22,27 +22,54 @@ function MusicalCupidCarousel() {
     const token = localStorage.getItem('token')
     const [items, setItems] = useState([]);
     const [artistid, setArtistid] = useState([]);
-    localStorage.setItem('idartist', JSON.stringify(artistid))
+    localStorage.setItem('idartist', JSON.stringify(artistid));
+    const idinfo = localStorage.getItem('cupidPlaylist');
+    const [playlistid, setPlaylistid] = useState();
+   
 
     useEffect(()=>{
-      var myHeaders = new Headers();
-      myHeaders.append("Authorization", token);
+      
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", token);
 
-     var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
+      var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
 
-    fetch("http://localhost:8000/api/artists", requestOptions)
-      .then(response => response.json())
-      .then(result => setItems(result.resultado))
-      .catch(error => console.log('error', error));
+      fetch("http://localhost:8000/api/artists", requestOptions)
+        .then(response => response.json())
+        .then(result => setItems(result.resultado))
+        .catch(error => console.log('error', error));
 
+
+        const getPlaylistByName = async ()=>{
+          var myHeaders = new Headers();
+          myHeaders.append("Authorization", token);
+          myHeaders.append("Content-Type", "application/json");
+
+         var requestOptions = {
+           method: 'GET',
+           headers: myHeaders,
+           redirect: 'follow'
+         };
+
+         fetch(`http://localhost:8000/api/playlistByName?playlistname=${idinfo}`, requestOptions)
+            .then(response => response.json())
+            .then(result => setPlaylistid(result.resultado))
+            .catch(error => console.log('error', error));
+
+          console.log(playlistid)
+          localStorage.setItem('idplaylist', JSON.stringify(playlistid[0].id_playlist));
+        }
+        getPlaylistByName();
+
+        console.log(idinfo)
+      
+    },[]) 
     
-    },[1]) 
     
-   
     const updateIndex = (newIndex) =>{
         if(newIndex < 0){
             newIndex = 0;
@@ -59,22 +86,62 @@ function MusicalCupidCarousel() {
       }else{
         setSongSelected( [...songSelected, items[activeIndex]])
         setArtistid([...artistid, items[activeIndex].id_artist])
-        
+        addSongToPlaylistCupid(items[activeIndex].id_artist);
       }
       
     };
    
+    const addSongToPlaylistCupid = async(artistids)=>{
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", token);
+      myHeaders.append("Content-Type", "application/json");
+
+      const artistInfoToAdd = {playlistid: playlistid[0].id_playlist,
+                               artist: artistids}
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(artistInfoToAdd),
+        redirect: 'follow'
+      };
+
+      fetch("http://localhost:8000/api/inserIntoPlaylistByArtist", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
     
   const handleLikeOrDislike = (event) => {
     event.preventDefault();
 
     setShowModal(true);
   };
+  const GobackHome = ()=>{
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", token);
+    myHeaders.append("Content-Type", "application/json");
+
+    const deletePlaylist = {idplaylist:playlistid[0].id_playlist }
+
+
+    var requestOptions = {
+      method: 'DELETE',
+      headers: myHeaders,
+      body: JSON.stringify(deletePlaylist),
+      redirect: 'follow'
+    };
+
+    fetch("http://localhost:8000/api/deletePlaylistById", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
 
   return (
     <>
         <header className='MusicalCupidHeader'>
-            <Link to={'/Home'}><button className='MusicalCupidGoHome'><img src={arrowGoHome}/></button></Link>
+            <Link to={'/Home'}><button onClick={GobackHome} className='MusicalCupidGoHome'><img src={arrowGoHome}/></button></Link>
       
             <h2 className='MusicalCupidCarouselTitle'>Cupido Musical</h2>
            
@@ -118,6 +185,7 @@ function MusicalCupidCarousel() {
     <p className='MusicalCupidCupidMatches'>Matches actuales: </p>
     <div className='MusicalCupidSongsLiked'>
      { songSelected.map((song)=>{
+      
         return <MusicalCupidCarouselSelected song={song}/>
         
      })
